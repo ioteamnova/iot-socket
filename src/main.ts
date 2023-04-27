a4940924b8ad51b4b460f611a7f99cab319f383a
Ubuntu <ubuntu@ip-172-31-35-18.ap-northeast-2.compute.internal>
2023-04-27 08:55:03 +0000

    mqtts 적용

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,29 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from "@nestjs/common";
 import { MicroserviceOptions } from '@nestjs/microservices';
 import { Transport } from '@nestjs/microservices/enums';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
+  const clientKey = readFileSync('/etc/mosquitto/CA/client_key.pem');
+  const clientCert = readFileSync('/etc/mosquitto/CA/client_crt.crt');
+  const caCert = readFileSync('/etc/mosquitto/CA/ca.crt');
 
     //subscribe시 연결
     const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
       transport: Transport.MQTT,
       options: {
         url: process.env.SERVERHOST,
+        clientId: 'server_nestjs',
         protocol: 'mqtt',
+        rejectUnauthorized: false,
         //username : 'ioteamnova',
         //password : '1234',
         subscribeOptions:{
           qos: 2,
         },
+        key: clientKey,
+        cert: clientCert,
+        ca: [caCert],
       },
     });
 
